5066e6b675fbaa15a54c19a4f5ea3a6862a72f73
NielGrobler <2690034@students.wits.ac.za>
2025-03-10 15:14:05 +0200

    More functionality, still incomplet

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
-document.getElementById("country").innerHTML = "Hello JavaScript!";
+(function () {
+    const input = document.getElementById("country");
+    const button = document.getElementById("submitButton");
+    button.addEventListener('click', () => loadData());
+})();
+
+function loadData() {
+    var mainCountryInfo = document.getElementById("country-info");
+    var otherCountriesInfo = document.getElementById("bordering-countries");
+    
+    getData("South Africa").then(function (result) {
+        const jsonMain = result; // Converting promise into 
+        console.log(jsonMain)
+
+        
+        
+        const mainList = document.createElement("ul");
+        const newContent = document.createTextNode("Hi there and greetings!");
+        mainList.appendChild(newContent)
+        mainCountryInfo.innerHTML = mainList
+    });
+    
+    //error handling here
+    //const countryInfo = JSON.parse(jsonMain);
+    
+}
+
+function format(json) {
+    
+}
 
 async function getData(country) {
-    const url = "https://example.org/products.json";
+    const url = new URL(country, "https://restcountries.com/v3.1/name/"); //better url parsing needed
+    const request1 = new Request(url, {method: "POST"});
     try {
       const response = await fetch(url);
       if (!response.ok) {
@@ -9,9 +39,10 @@ async function getData(country) {
       }
   
       const json = await response.json();
-      console.log(json);
+      return json;
     } catch (error) {
-      console.error(error.message);
+        //console.error(error.message)
+      return error.message;
     }
   }
   
