f7e7b14e4d8384311d365c90bb67a054c4c21e5e
NielGrobler <2690034@students.wits.ac.za>
2025-03-10 14:19:39 +0200

    script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById("country").innerHTML = "Hello JavaScript!";
+
+async function getData(country) {
+    const url = "https://example.org/products.json";
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+      const json = await response.json();
+      console.log(json);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+  
